1b7f1cd99c4abfdc20a0f2b8392efaf83f2ab331
jasontrigg0 <jasontrigg0@gmail.com>
2015-12-20 03:39:57 -0500

    preparing for pypi submission

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'python-snake',
+    packages = ['python-snake'],
+    version = '0.1',
+    description = 'Data workflow tool (rough copy of drake for python)',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/snake",
+    download_url = 'https://github.com/jasontrigg0/snake/tarball/0.1',
+    keywords = [],
+    classifiers = [],
+)
