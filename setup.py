4edc116556c5fbdda63f644661c1feab7e229fb9
jasontrigg0 <jasontrigg0@gmail.com>
2016-02-13 13:44:53 -0500

    pypi version 0.0.6

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
     name = 'python-snake',
-    packages = ['python-snake'],
+    packages = ['snake'],
     version = "0.0.5",
     description = 'Data workflow tool (rough copy of drake for python)',
     author = "Jason Trigg",
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/snake",
     download_url = 'https://github.com/jasontrigg0/snake/tarball/0.0.5',
-    scripts=['python-snake/snake'],
+    scripts=['snake/snake'],
     keywords = [],
     classifiers = [],
 )
